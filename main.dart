80b6e2661ed6963d88f153db48f631464faf0a7a
codewithwajihaa <137100693+codewithwajihaa@users.noreply.github.com>
2023-06-25 10:01:10 -0700

    FIRST COMIT

---
+++ main.dart
@@ -0,0 +1,164 @@
+// ################# QUESTION =1 #####################
+// void main() {
+//   var list1 = ["wajiha", "fatima", "sumbul", "mehak"];
+//   print(list1);
+// }
+
+// #################### QUESTION =2 #####################
+void main() {
+  // var list2 = [];
+  // if (list2.isEmpty) {
+  //   print("Empty list found");
+  // }
+  // list2.add("momday");
+  // list2.add("tuesday");
+  // list2.add("wednesday");
+  // list2.add("thursday");
+  // list2.add("friday");
+  // list2.add("saturday");
+  // list2.add("sunday");
+  // print(list2);
+  // if (list2.isNotEmpty) {
+  //   print("List is Not Empty");
+  // }
+
+// ################### QUESTION =3 ##############################
+
+  // var list3 = [
+  //   "MONDAY",
+  //   "TUESDAY",
+  //   "WEDNESDAY",
+  //   "THURSDAY",
+  //   "FRIDAY",
+  //   "SATURDAY",
+  //   "SUNDAY"
+  // ];
+  // list3.remove("sunday");
+  // print(list3);
+  // list3.remove("saturday");
+  // print(list3);
+  // list3.remove("friday");
+  // print(list3);
+  // list3.remove("thursday");
+  // print(list3);3
+  // list3.remove("wednesday");
+  // print(list3);
+  // list3.remove("tuesday");
+  // print(list3);
+  // list3.remove("monday");
+  // print(list3);
+  // list3.clear();
+  // print(list3);
+
+// ####################### QUESTION = 4 ##################
+  // var list4 = [1, 2, 3, 44, 88, 99, 100];
+  // print(list4.last);
+// ######################## QUESTION =5  #######################
+  // var map1 = {1: "fatima", 2: "wajiha", 3: "zainab", 4: "filza"};
+  // print(map1.length);
+
+// ############################# QUESTION= 6#######################
+  // var map2 = {1: "Pakistan", 2: "islamabad", 3: "rupees", 4: "urdu"};
+  // print(map2);
+
+// ###################### QUESTION =7##########################
+  // var map3 = {'sun': 3000.0, 'mon': 3000.0, 'tue': 32440.0};
+  // map3['fri'] = 5000.0;
+  // print(map3);
+
+// // #################### QUESTION =8###################
+//  varlist>map<string,bool
+//  =[{"name":'John','eligible':true},{"name":'Alice','eligible':false},{"name":'Mike','eligible':'true'},{"name":'sarah','eligible':'true'},{"name";'Tom':'eligible':'true'}];
+// print(list>map<string,bool);
+
+// #################### QUESTION = 9 #############################
+
+  // var list6 = [-1, -2, -3, -78];
+//   // print(list6.first);
+
+// ################## QUESTION = 10 ##########################
+  // var list7 = ["lays", "pencil", "rubber", "scale"];
+  // list7.remove("lays");
+  // print(list7);
+// ####################  QUESTION = 11 ################
+
+  // var list8 = [9, 6, 4, 3];
+  // list8.replaceRange(2, 4, [88,100]);
+  // print(list8);
+
+// ################# QUESTION = 12 ######################
+
+  // List<dynamic> studentNames = ["john", "einstein", "keats", "percy"];
+  // var reversedList = List.of(studentNames.reversed);
+  // print(reversedList);
+// ######################## QUESTION = 13 ###################
+
+//   var list1 = [1, 9, 4, 7];
+//   list1.insert(2,"ayesha");
+//   print(list1);
+// ###################   QUESTION = 14 ###################
+
+  // var list2 = <int>[2, 3, -22, 55, 66, 922];
+  // list2.sort();
+  // print(list2);
+
+// #################### QUESTION = 15 ######################
+
+  // var list3 = [1, -2, 4, 3, 9, 10];
+  // list3.removeRange(1, 2);
+  // print(list3);
+
+//
+// ###################### QUESTION = 16 ######################
+// var list6 =[1,2,3,4,5,6];
+// list6.remove(2,4,6);
+// // print(list6);
+
+// ############### QUESTION =17 ########################
+  // var list1 = [1, 22, 44, 45, 32, 6];
+  // var map2 = [];
+  // if (map2.isEmpty) {
+  //   print(Empty list found);
+
+// ##################### QUESTION =18 ########################
+
+// // map person ={"name":'john',"occupation":"gardner","age":20,'isStudent':true,};;
+//   if (person == 0 && person >= 18 == 0) {
+//     print("eligible");
+//   } else {
+//     print("not eligible");
+  }
+// // ############### QUESTION =19 ##########################
+//   map product = {
+//     "name": "cotton suit",
+//     "price": 3000,
+//     "quantity": 20,
+//   };
+//   int quality = product["cotton suit"];
+//   if (quality > 0) {
+//     print("Instock");
+//   } else {
+//     print("out of stock");
+//   }#
+// // ################## QUESTION = 20 #########################
+// var map6 car ={"brand":'toyota',"color":'red',"isSadan":true};
+// print(map6);
+// if(the car is red or sadan){print (red or sadan );}
+// else{the car is not red};
+
+// // ########################  QUESTION = 21 ########################
+  // map user = {
+  //   "name": "adam",
+  //   "isAdmin": true,
+  //   "isActive": true,
+  // };
+  // bool  isActiveAdmin= user{"isAdmin"} && user{"isActive"};
+  // if( isActiveAdmin){print("ActiveAdmin");}
+  // else{print("not an ActiveAdmin");}
+
+// ##############################  QUESTGION = 22 ####################
+// map shopping cart ={"Apple":5,"Custard Apple":8,"orange":4,};
+// bool product found= shopping cart.containkey("apple");
+// if(product found){print("product found");}
+// else{print("product not found");}
+}
